db115d66af51bb33f4d34310ba7b02c007624c45
bathant702 <bathant702nv@gmail.com>
2023-08-04 01:08:32 -0700

    updated info

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -263,6 +263,20 @@ console.log(friends.includes('Hugo')); //true
 
 
 // // challenge #2: a better tip calculator
+/*
+CHALLENGE #2
+Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+And now let's use arrays! So, create an array called bills containing the test data below.
+Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+TEST DATA: 125, 555, and 44.
+
+BONUS: Create an array totals containing the total values, so the bill + tip.
+
+*/
 // my solution - incomplete
 // const bills = [125, 555, 44];
 const bills = [100, 200]
@@ -283,3 +297,13 @@ console.log(tips);
 // //written as an arrow function
 // const calcTipArrow = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
+// //with bonus challenge
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+ 
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+ 
+// console.log(bills, tips, totals);
