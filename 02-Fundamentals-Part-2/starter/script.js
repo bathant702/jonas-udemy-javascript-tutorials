5f71c561f327001df0bb2c99478f900d1d957224
bathant702 <bathant702nv@gmail.com>
2023-08-09 12:20:38 -0700

    arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -279,10 +279,10 @@ BONUS: Create an array totals containing the total values, so the bill + tip.
 */
 // my solution - incomplete
 // const bills = [125, 555, 44];
-const bills = [100, 200]
-const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-const tips = [calcTip(bills[0]), calcTip(bills[1])];
-console.log(tips);
+// const bills = [100, 200]
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const tips = [calcTip(bills[0]), calcTip(bills[1])];
+// console.log(tips);
 
 // official solution
 // // written as regular function expression
@@ -307,3 +307,25 @@ console.log(tips);
 // const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
  
 // console.log(bills, tips, totals);
+
+
+// // lecture: introduction to objects
+
+const timArray = [
+  'Tim',
+  'Bathan',
+  2023 - 1986,
+  'aspiring web developer',
+  ['Matt', 'Sara', 'Andrew']
+];
+
+const tim = {
+  firstName: 'Tim',
+  lastName: 'Bathan',
+  age: 2023 - 1986,
+  job: 'aspring web developer',
+  family: ['Matt', 'Sara', 'Andrew']
+};
+
+console.log(tim);
+console.log(timArray);
