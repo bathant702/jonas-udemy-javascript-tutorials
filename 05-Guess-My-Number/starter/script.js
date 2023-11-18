7a5d2d029903796b1631baf44490b5cc997b4ce3
bathant702 <bathant702nv@gmail.com>
2023-11-17 22:14:03 -0800

    programming game ai

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,7 +4,7 @@
 console.log(document.querySelector('.message').textContent);
 // this piece of code selects the element with class="message". it starts with document.querySelector('.message'), with "." signifying class. If we wanted to do with an ID, we'd use "#" instead. the .textContent selects the text in that element.
 
-document.querySelector('.message').textContent = 'Correct Number!';
+
 
 console.log(document.querySelector('.message').textContent);
 
@@ -15,8 +15,12 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+//rnd number
+let score = 20;
+let highscore;
+document.querySelector('.number').textContent = secretNumber;
+//user guessing
 
 document.querySelector('.check').addEventListener('click', function () {
   //using quesrySelector to get the value of the guess input whenever a class with 'check' is clicked
@@ -30,5 +34,33 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     //!guess means "if guess is 0/false, then do a thing"
     document.querySelector('.message').textContent = 'No number!';
+    //if there is no number
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    //if it is the correct number
+    document.querySelector('.score').textContent = highscore;
+    //records your high score
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+    //if it is too high
+    score--;
+    document.querySelector('.score').textContent = score;
+    //minus your score
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too low!';
+      //if it is too low
+      score--;
+      document.querySelector('.score').textContent = score;
+      //minus your score
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game.';
+      //if score is 0
+      let newSecretNumber = Math.trunc(Math.random() * 20) + 1;
+      newSecretNumber = secretNumber;
+      score = 20;
+      document.querySelector('.score').textContent = score;
+      //reset?
+    }
   }
 });
