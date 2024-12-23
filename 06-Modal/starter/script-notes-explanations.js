22f39314105a5a62480499c0011535428a9facec
bathant702 <bathant702nv@gmail.com>
2024-12-22 21:38:10 -0800

    learned about modals and how to manipulate them using JS to add and remove classes from them at will.

--- 06-Modal/starter/script-notes-explanations.js
+++ 06-Modal/starter/script-notes-explanations.js
@@ -1,1 +1,37 @@
 'use strict';
+
+//variables to use
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+//check to see if opening the modals works in console
+console.log(btnOpenModal);
+
+//created a loop to be able to apply function to all btns
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked'); //confirms button was clicked
+    modal.classList.remove('hidden'); //no "." when targeting the class. that is only for the selector. in this case, we're just eliminating the class
+    overlay.classList.remove('hidden'); //hides the menu behind, blurring it.
+  });
+//the above can be refactored exactly like the close modal function
+
+
+//This was the original approach of creating a function to open and close a window. from there, it was replicated to both the modal window and overla
+btnCloseModal.addEventListener('click', function () {\
+  //to close modal window via "X"
+  modal.classList.add('hidden');
+  //to close modal window via "outside of window"
+  overlay.classList.add('hidden');
+});
+
+//This is the above code, refractored to be more DRY. the operation of opening and closing the window was turned into a named function, simplifying the process and code block
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//by creating a function, you can refactor the code, making it easier and neater
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden'); //no "." only for the selector. in this case, we're just eliminating the class
+  overlay.classList.remove('hidden'); //hides the menu behind, blurring it.
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//created a loop to be able to apply function to all btns
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
