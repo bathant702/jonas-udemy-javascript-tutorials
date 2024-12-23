bbea2893e4d8c90dc7361454f93927d90d7709d6
bathant702 <bathant702nv@gmail.com>
2024-12-22 21:39:58 -0800

    cleaned up starter code

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,24 +1,27 @@
 'use strict';
 
+//variables
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
 
+//opening a modal
 const openModal = function () {
-  modal.classList.remove('hidden'); //no "." only for the selector. in this case, we're just eliminating the class
-  overlay.classList.remove('hidden'); //hides the menu behind, blurring it.
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
+//closing a modal
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-//created a loop to be able to apply function to all btns
+//loop to target all buttons for modals
 for (let i = 0; i < btnOpenModal.length; i++)
   btnOpenModal[i].addEventListener('click', openModal);
 
+//open and close of modal
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
